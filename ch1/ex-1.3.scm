14317362e626a082c488465c056cf1a999c9fd03
Saurav Sachidanand <sauravsachidanand@gmail.com>
2016-08-05 21:57:47 +0530

    Add ex-1.3

---
+++ ch1/ex-1.3.scm
@@ -0,0 +1,11 @@
+(define (square x) (* x x))
+(define (sum-of-squares x y) (+ (square x) (square y)))
+
+(define (f a b c)
+        (if (> a b)
+            (if (> c b)
+                (sum-of-squares a c)
+                (sum-of-squares a b))
+            (if (> c a)
+                (sum-of-squares b c)
+                (sum-of-squares b a))))
